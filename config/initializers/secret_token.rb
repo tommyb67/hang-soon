8b46d861f594e160724cfbd57449cbeac48f2019
Tom Brennan <tommybrennan@gmail.com>
2014-12-12 15:59:54 -0500

    initial ommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HangSoon::Application.config.secret_key_base = '69d35fc6b72f95cc3205dbbb110db1a5aada53a0dc44daf8c825efac06514686539bb6f4ac2f963de0d79e59f6d22e4aa202ed9450255bb585ae751a49e64fdb'
